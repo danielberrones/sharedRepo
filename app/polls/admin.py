d2adc088e324d7b49d9de6a0ed30ba8b5df5c924
danielberrones <daniel.a.berrones@gmail.com>
2023-02-10 23:26:34 -0600

    refactored

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -1,7 +1,27 @@
-from django.contrib import admin
-
-from .models import Question
-
-admin.site.register(Question)
-
+#DEFAULT DJANGO TEXT
+#from django.contrib import admin
+#from .models import Question
+#admin.site.register(Question)
 # Register your models here.
+#DEFAULT DJANGO TEXT
+
+
+#dbStart
+from django.contrib import admin
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+#dbEnd
